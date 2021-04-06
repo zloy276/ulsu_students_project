bf2befbdbe457381547829f433734f11ef746ddc
FreakingJackpot <70316335+FreakingJackpot@users.noreply.github.com>
2021-04-07 01:31:57 +0400

    view and urls are cool

--- server/nlp/urls.py
+++ server/nlp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.model_form_upload, name='upload'),
-
+    path('show/<int:pk>', views.process_doc, name='show'),
 ]
--- server/nlp/views.py
+++ server/nlp/views.py
@@ -1,8 +1,8 @@
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
-
-from .models import Student
+from django.urls import reverse
+from .models import Student,Document
 
 
 def model_form_upload(request):
@@ -10,9 +10,15 @@ def model_form_upload(request):
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            doc=Document.objects.all().order_by("-uploaded_at")[0]
+            return redirect(reverse('show',kwargs={"pk":doc.id}))
     else:
         form = DocumentForm()
     return render(request, 'upload.html', {
         'form': form
     })
+
+def process_doc(request,pk):
+    doc=Document.objects.get(pk=pk)
+    print(doc.document)
+    return render(request,'show.html')
