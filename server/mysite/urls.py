6093c246f1eb8399c7638154f26d0fd4ef8c7546
zloy <mihan276@icloud.com>
2021-04-06 23:26:01 +0400

    fix

--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('nlp.urls'))
 ]
-
+# test
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
