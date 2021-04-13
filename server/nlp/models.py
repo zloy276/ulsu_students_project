6957027cf7b8abd57be8f130beaecbe2812f0752
Jackpot <sss30112001@yandex.ru>
2021-04-13 18:46:36 +0400

    models

--- server/nlp/models.py
+++ server/nlp/models.py
@@ -3,13 +3,29 @@ from django.db import models
 
 
 # Create your models here.
+class Faculty(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+
+
+class Direction(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    faculty = models.ForeignKey(
+        'Faculty', on_delete=models.CASCADE, verbose_name='Факультет')
+
 
 class Student(models.Model):
     full_name = models.CharField("ФИО", max_length=200, db_index=True)
-    faculty = models.CharField("Факультет", max_length=200)
-    directon = models.CharField("Направление", max_length=200)
+    faculty = models.ForeignKey(
+        'Faculty', on_delete=models.CASCADE, verbose_name="Факультет")
+    directon = models.ForeignKey(
+        'Direction', on_delete=models.CASCADE, verbose_name='Направление')
     profile = models.CharField("Профиль", max_length=200)
     topic = models.CharField("Тема ВКР", max_length=200)
+    words_cloud = ArrayField(models.CharField(
+        max_length=200), blank=True, verbose_name="Облако слов")
+    document = models.ForeignKey('Document', on_delete=models.CASCADE)
+    processed_document = models.ForeignKey(
+        'Processed_document', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('full_name',)
@@ -24,6 +40,15 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.document.name
+
+
+class Processed_document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='processed_documents/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.document.name
