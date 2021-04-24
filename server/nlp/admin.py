56c5b206fdff4d83ec76494003d31884eb4b249e
zloy <mihan276@icloud.com>
2021-04-24 13:53:01 +0400

    fix

--- server/nlp/admin.py
+++ server/nlp/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from . import models
+from django.contrib import admin
+from django.contrib.postgres.fields import JSONField
+from .utils import ReadableJSONFormField
+from .models import Some
+
+@admin.register(models.Logs)
+class SomeAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        JSONField: {'form_class': ReadableJSONFormField},
+    }
 
 admin.site.register(models.Document)
 admin.site.register(models.Student)
