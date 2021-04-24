8c9a68358cabf6c04dc9f5a12942f6e3b17923a6
zloy <mihan276@icloud.com>
2021-04-24 13:18:10 +0400

    fix

--- server/nlp/signals.py
+++ server/nlp/signals.py
@@ -9,5 +9,7 @@ def model_m2m_changed(*args, **kwargs):
     instance = kwargs.get("instance")
     info_arr = [instance.full_name, instance.words_cloud, instance.direction.name, instance.direction.department.name,
                 instance.direction.department.faculty.name]
-    if 'Error' in info_arr:
+    if 'Error' in info_arr and instance.is_normal == True:
+        instance.is_normal = False
         Logs.objects.create(student=instance, info=json.dumps(instance.__dict__, default=str))
+        instance.save()
