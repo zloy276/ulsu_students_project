43a2d8a26d280c072a7de5503d387da15fc6a9f2
zloy <mihan276@icloud.com>
2021-05-12 22:24:25 +0400

    fix

--- server/nlp/management/commands/load_this.py
+++ server/nlp/management/commands/load_this.py
@@ -13,12 +13,15 @@ from nlp.models import UploadedFile
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open(os.path.join(os.getcwd(), 'file.csv'), "r") as f_obj:
-            reader = csv.reader(f_obj)
-            mas = [row[0] for row in reader]
-            files = os.listdir('{}/media/vkr'.format(settings.BASE_DIR))
-            print(files)
-            for file in files:
-                if file in mas:
-                    with open('{}/media/vkr/{}'.format(settings.BASE_DIR, file), "rb") as f:
-                        UploadedFile.objects.create(document=ContentFile(f.read(),name=file))
+        # with open(os.path.join(os.getcwd(), 'file.csv'), "r") as f_obj:
+        #     reader = csv.reader(f_obj)
+        #     mas = [row[0] for row in reader]
+        #     files = os.listdir('{}/media/vkr'.format(settings.BASE_DIR))
+        #     print(files)
+        #     for file in files:
+        #         if file in mas:
+        #             with open('{}/media/vkr/{}'.format(settings.BASE_DIR, file), "rb") as f:
+        #                 UploadedFile.objects.create(document=ContentFile(f.read(),name=file))
+        for i in UploadedFile.objects.all():
+            i.is_processed = False
+            i.save()
