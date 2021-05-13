a87443c226e8f5829dfded60164103c7307bc475
FreakingJackpot <sss30112001@yandex.ru>
2021-05-13 22:33:38 +0400

    jopa

--- server/nlp/management/commands/govno.py
+++ server/nlp/management/commands/govno.py
@@ -31,7 +31,7 @@ class Command(DaemonCommand):
                 continue
             l.append(t)
         for i in l:
-            file_name = i['file_name'].split('.')
+            file_name = i['FILE_NAME'].split('.')
             file_name = file_name[0]+file_name[1].lower()
             doc = UploadedFile.objects.filter(is_processed=False, document='documents/'+file_name)
 
